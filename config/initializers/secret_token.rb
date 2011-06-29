2dce791b08d9fe5b8ddb4f94e0448abc2437bfb4
alban vervust <alban.vervust@gmail.com>
2011-06-29 22:36:08 +0200

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cannelle::Application.config.secret_token = '9ed2a92a5f21da77cabd4767c1974306d922d760cc9cbba33ee72e1e8664f0f0e10ea2ee8899977755d6947be3c3851da3f3687d15277c5d23de4edcbe6dceb0'
