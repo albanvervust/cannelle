b40a5782f5dece0f5424894c5f83dc39731fd617
alban vervust <alban.vervust@gmail.com>
2011-08-17 13:38:25 +0200

    solved problem of convives

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,9 +3,4 @@ class Invitation < ActiveRecord::Base
   has_and_belongs_to_many :ambiances
   validates_presence_of :name, :date, :description, :convives, :price
   
-  def convives=(integer)
-    Self.convives = Convives.find_by_integer(integer) unless integer.blank?
-  end
-  
-  
 end
